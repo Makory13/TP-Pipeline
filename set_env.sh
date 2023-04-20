3a04b5a06c99a9c892f5f99cb4c7d4d4fb1a15bf
Yohan Stoecklé <45103837+Makory13@users.noreply.github.com>
2023-04-20 15:51:22 +0200

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=adminuser
+export POSTGRES_PASSWORD=admin123
+export POSTGRES_HOST=udagramd-db
+export POSTGRES_DB=testdb
+export AWS_BUCKET=udagram-ynov-ygd
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
